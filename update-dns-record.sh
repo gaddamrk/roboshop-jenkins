8f2a5d58d30e59956e077e386251a48e3f5a35c6
raj <raj>
2023-06-26 15:29:22 +0530

    set roboshop m

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -1,6 +1,6 @@
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins2" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
-echo'
+echo '
 {
   "Comment": "CREATE/DELETE/UPSERT a record ",
   "Changes": [{
