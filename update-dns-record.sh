28854b33627904de498786f20ee38a1ed5ff199e
raj <raj>
2023-06-26 15:34:50 +0530

    set roboshop m

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -11,4 +11,8 @@ echo '
       "TTL": 15,
       "ResourceRecords": [{ "Value": "IPADDRESS"}]
     }}]
-}' | sed -e "s/IPADDRESS/${IP}/"
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins2.json
+
+ZONE_ID="Z026433835I4XRBDKYCNA"
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+
